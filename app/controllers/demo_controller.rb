adb739ac44a62f248572bfbf06fcee62a1479843
Andrew Kirschke <andrew.kirshcke@gmx.com>
2016-05-22 20:07:55 -0400

    first commit

---
+++ app/controllers/demo_controller.rb
@@ -0,0 +1,18 @@
+class DemoController < ApplicationController
+  layout false
+
+  def index
+
+  end
+
+  def hello
+    @hello = "Ohai!"
+    @array = [1,2,3,4,5]
+    @id = params['id']
+    @page = params[:page].to_i
+  end
+
+  def other_hello
+    redirect_to(controller: 'demo', action: 'hello')
+  end
+end
